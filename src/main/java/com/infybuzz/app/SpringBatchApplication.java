1e605b36c223dcc290d835cee44724ce61556290
mac <thienvy95@gmail.com>
2026-01-12 18:49:44 +0700

    [feat] job listener

--- src/main/java/com/infybuzz/app/SpringBatchApplication.java
+++ src/main/java/com/infybuzz/app/SpringBatchApplication.java
@@ -1,7 +1,5 @@
 package com.infybuzz.app;
 
-import java.util.Map;
-
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,16 +7,15 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableBatchProcessing
-@ComponentScan({ "com.infybuzz.config", "com.infybuzz.service" })
+@ComponentScan({ "com.infybuzz.config", "com.infybuzz.service", "com.infybuzz.listener" })
 public class SpringBatchApplication {
 
 	public static void main(String[] args) {
-		  String[] fakeArgs = {
-            "run=one",
-            "time=1"
-        };
+		String[] fakeArgs = {
+				"run=two"
+		};
 
-        SpringApplication.run(SpringBatchApplication.class, fakeArgs);
+		SpringApplication.run(SpringBatchApplication.class, fakeArgs);
 	}
 
 }
