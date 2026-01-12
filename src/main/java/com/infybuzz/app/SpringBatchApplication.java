1a8736ea37769ba9764218033c9683d276962e04
mac <thienvy95@gmail.com>
2026-01-12 15:33:05 +0700

    [feat] fake a args for testing BATCH_JOB_INSTANCE

--- src/main/java/com/infybuzz/app/SpringBatchApplication.java
+++ src/main/java/com/infybuzz/app/SpringBatchApplication.java
@@ -1,5 +1,7 @@
 package com.infybuzz.app;
 
+import java.util.Map;
+
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,11 +9,16 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableBatchProcessing
-@ComponentScan({"com.infybuzz.config", "com.infybuzz.service"})
+@ComponentScan({ "com.infybuzz.config", "com.infybuzz.service" })
 public class SpringBatchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBatchApplication.class, args);
+		  String[] fakeArgs = {
+            "run=one",
+            "time=1"
+        };
+
+        SpringApplication.run(SpringBatchApplication.class, fakeArgs);
 	}
 
 }
